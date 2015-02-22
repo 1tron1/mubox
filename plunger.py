81afc44c486ff84c6c5d9242dc77aea139cb7024
Daniel McCormack <djmccormack6@gmail.com>
2015-02-21 19:59:53 -0500

    Commito Uno!
    Basics of the Mubox just data not auto analysis software

---
+++ plunger.py
@@ -0,0 +1,13 @@
+import serial
+addr  = 'COM4'
+baud  = 9600
+fname = 'vals.dat'
+fmode = 'ab'
+reps  = 10
+
+with serial.Serial(addr,baud) as port, open(fname,fmode) as outf:
+    for i in range(reps):
+        x = port.read(size=1)
+        print (x)
+        outf.write(x)
+        outf.flush()
